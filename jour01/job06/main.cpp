c89dc98f934b48f673d913c051a924c3a40c5114
walid-saadelkhalk <walid.saad-el-khalid@laplateforme.io>
2024-04-29 21:10:47 +0200

    [CREATE] job 4 done: this program permits to input a number and give it specific multiply table

---
+++ jour01/job06/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int N;
+    std::cout << "Entrez la valeur de N : ";
+    std::cin >> N;
+    std::cout << N << "x" << 1 << "=" << N * 1 << std::endl;
+    std::cout << N << "x" << 2 << "=" << N * 2 << std::endl;
+    std::cout << N << "x" << 3 << "=" << N * 3 << std::endl;
+    std::cout << N << "x" << 4 << "=" << N * 4 << std::endl;
+    std::cout << N << "x" << 5 << "=" << N * 5 << std::endl;
+    std::cout << N << "x" << 6 << "=" << N * 6 << std::endl;
+    std::cout << N << "x" << 7 << "=" << N * 7 << std::endl;
+    std::cout << N << "x" << 8 << "=" << N * 8 << std::endl;
+    std::cout << N << "x" << 9 << "=" << N * 9 << std::endl;
+    std::cout << N << "x" << 10 << "=" << N * 10 << std::endl;
+}
